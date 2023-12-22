aa537153d45c3e4131bdb314e937d8fe567b6815
Güray Gül <guraygul59@gmail.com>
2023-12-22 15:30:20 +0300

    Refactored the code and added Restaurant struct

---
+++ FoodPin/Restaurant.swift
@@ -0,0 +1,16 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Güray Gül on 22.12.2023.
+//
+
+import Foundation
+
+struct Restaurant: Hashable {
+    var name: String = ""
+    var type: String = ""
+    var location: String = ""
+    var image: String = ""
+    var isFavorite: Bool = false
+}
--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -13,27 +13,29 @@ class RestaurantTableViewController: UITableViewController {
         case all
     }
     
-    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster",
-                           "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery",
-                           "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif",
-                           "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore",
-                           "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
-    
-    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster",
-                            "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino",
-                            "upstate", "traif", "graham", "waffleandwolf", "fiveleaves",
-                            "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
-    
-    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong",
-                               "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney",
-                               "New York", "New York", "New York", "New York", "New York",
-                               "New York", "New York", "London", "London", "London", "London"]
-    
-    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian/ Causual Drink",
-                           "French", "Bakery", "Bakery", "Chocolate", "Cafe",
-                           "American / Seafood", "American", "American", "Breakfast & Brunch",
-                           "Coffee &Tea", "Coffee & Tea", "Latin American", "Spanish",
-                           "Spanish", "Spanish", "British", "Thai"]
+    var restaurants:[Restaurant] = [
+        Restaurant(name: "Cafe Deadend", type: "Coffee & Tea Shop", location:"Hong Kong", image: "cafedeadend", isFavorite: false),
+        Restaurant(name: "Homei", type: "Cafe", location: "Hong Kong", image:"homei", isFavorite: false),
+        Restaurant(name: "Teakha", type: "Tea House", location: "Hong Kong", image: "teakha", isFavorite: false),
+        Restaurant(name: "Cafe loisl", type: "Austrian / Causual Drink", location: "Hong Kong", image: "cafeloisl", isFavorite: false),
+        Restaurant(name: "Petite Oyster", type: "French", location: "Hong Kong", image: "petiteoyster", isFavorite: false),
+        Restaurant(name: "For Kee Restaurant", type: "Bakery", location: "HongKong", image: "forkee", isFavorite: false),
+        Restaurant(name: "Po's Atelier", type: "Bakery", location: "Hong Kong", image: "posatelier", isFavorite: false),
+        Restaurant(name: "Bourke Street Backery", type: "Chocolate", location:"Sydney", image: "bourkestreetbakery", isFavorite: false),
+        Restaurant(name: "Haigh's Chocolate", type: "Cafe", location: "Sydney", image: "haigh", isFavorite: false),
+        Restaurant(name: "Palomino Espresso", type: "American / Seafood", location: "Sydney", image: "palomino", isFavorite: false),
+        Restaurant(name: "Upstate", type: "American", location: "New York", image: "upstate", isFavorite: false),
+        Restaurant(name: "Traif", type: "American", location: "New York", image: "traif", isFavorite: false),
+        Restaurant(name: "Graham Avenue Meats", type: "Breakfast & Brunch", location: "New York", image: "graham", isFavorite: false),
+        Restaurant(name: "Waffle & Wolf", type: "Coffee & Tea", location: "NewYork", image: "waffleandwolf", isFavorite: false),
+        Restaurant(name: "Five Leaves", type: "Coffee & Tea", location: "New York", image: "fiveleaves", isFavorite: false),
+        Restaurant(name: "Cafe Lore", type: "Latin American", location: "New York", image: "cafelore", isFavorite: false),
+        Restaurant(name: "Confessional", type: "Spanish", location: "New York", image: "confessional", isFavorite: false),
+        Restaurant(name: "Barrafina", type: "Spanish", location: "London", image: "barrafina", isFavorite: false),
+        Restaurant(name: "Donostia", type: "Spanish", location: "London", image: "donostia", isFavorite: false),
+        Restaurant(name: "Royal Oak", type: "British", location: "London", image: "royaloak", isFavorite: false),
+        Restaurant(name: "CASK Pub and Kitchen", type: "Thai", location: "London", image: "cask", isFavorite: false)
+    ]
     
     var restaurantIsFavorites = Array(repeating: false, count: 21)
     
@@ -46,9 +48,9 @@ class RestaurantTableViewController: UITableViewController {
         tableView.dataSource = dataSource
         tableView.separatorStyle = .none
         
-        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Restaurant>()
         snapshot.appendSections([.all])
-        snapshot.appendItems(restaurantNames, toSection: .all)
+        snapshot.appendItems(restaurants, toSection: .all)
         
         dataSource.apply(snapshot, animatingDifferences: false)
     }
@@ -80,7 +82,7 @@ class RestaurantTableViewController: UITableViewController {
         let reserveAction = UIAlertAction(title: "Reserve a table", style: .default, handler: reserveActionHandler)
         optionMenu.addAction(reserveAction)
         
-        let favoriteActionTitle = self.restaurantIsFavorites[indexPath.row] ? "Remove from favorite" : "Mark as favorite"
+        let favoriteActionTitle = self.restaurants[indexPath.row].isFavorite ? "Remove from favorite" : "Mark as favorite"
         
         let favoriteAction = UIAlertAction(title: favoriteActionTitle,
                                            style: .default,
@@ -89,9 +91,9 @@ class RestaurantTableViewController: UITableViewController {
             
             let cell = tableView.cellForRow(at: indexPath) as! RestaurantTableViewCell
             
-            cell.favoriteImageView.isHidden = self.restaurantIsFavorites[indexPath.row]
+            cell.favoriteImageView.isHidden = self.restaurants[indexPath.row].isFavorite
             
-            self.restaurantIsFavorites[indexPath.row].toggle()
+            self.restaurants[indexPath.row].isFavorite.toggle()
         })
         optionMenu.addAction(favoriteAction)
         
@@ -106,20 +108,20 @@ class RestaurantTableViewController: UITableViewController {
     //Mark as favorite action
     
     
-    func configureDataSource() -> UITableViewDiffableDataSource<Section, String> {
+    func configureDataSource() -> UITableViewDiffableDataSource<Section, Restaurant> {
         
         let cellIdentifier = "favoritecell"
         
-        let dataSource = UITableViewDiffableDataSource<Section, String>(
+        let dataSource = UITableViewDiffableDataSource<Section, Restaurant>(
             tableView: tableView,
-            cellProvider: {  tableView, indexPath, restaurantName in
+            cellProvider: {  tableView, indexPath, restaurant in
                 let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell
                 
-                cell.nameLabel.text = restaurantName
-                cell.thumbnailImageView.image = UIImage(named: self.restaurantImages[indexPath.row])
-                cell.locationLabel.text = self.restaurantLocations[indexPath.row]
-                cell.typeLabel.text = self.restaurantTypes[indexPath.row]
-                cell.favoriteImageView.isHidden = self.restaurantIsFavorites[indexPath.row] ? false : true
+                cell.nameLabel.text = restaurant.name
+                cell.thumbnailImageView.image = UIImage(named: restaurant.image)
+                cell.locationLabel.text = restaurant.location
+                cell.typeLabel.text = restaurant.type
+                cell.favoriteImageView.isHidden = restaurant.isFavorite ? false : true
                 
                 return cell
             }
