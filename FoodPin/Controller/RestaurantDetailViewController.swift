e6d9999ee918c31808b7c22f3ea738c187676fe7
Güray Gül <guraygul59@gmail.com>
2023-12-24 18:41:39 +0300

    Custom fonts added and detail view changed

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -8,36 +8,27 @@
 import UIKit
 
 class RestaurantDetailViewController: UIViewController {
-
-    @IBOutlet var restaurantImageView: UIImageView!
-    @IBOutlet var restaurantName: UILabel!
-    @IBOutlet var restaurantType: UILabel!
-    @IBOutlet var restaurantLocation: UILabel!
     
-//    var restaurantImageName = ""
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var headerView: RestaurantDetailHeaderView!
+    
     var restaurant = Restaurant()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        restaurantImageView.image = UIImage(named: restaurantImageName)
-        restaurantImageView.image = UIImage(named: restaurant.image)
-        restaurantName.text = restaurant.name
-        restaurantType.text = restaurant.type
-        restaurantLocation.text = restaurant.location
         
-        navigationItem.largeTitleDisplayMode = .never
+        navigationController?.navigationBar.prefersLargeTitles = false
+        
+        //Configure header view
+        headerView.nameLabel.text = restaurant.name
+        headerView.typeLabel.text = restaurant.type
+        headerView.headerImageView.image = UIImage(named: restaurant.image)
+        
+        let heartImage = restaurant.isFavorite ? "heart.fill" : "heart"
+        headerView.heartButton.tintColor = restaurant.isFavorite ? .systemYellow : .white
+        
+        headerView.heartButton.setImage(UIImage(systemName: heartImage), for: .normal)
+        
         
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
