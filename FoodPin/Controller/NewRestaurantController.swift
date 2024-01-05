50e681f19c3467b96069e2748ed813e6bbbd15ab
Güray Gül <guraygul59@gmail.com>
2024-01-05 16:47:14 +0300

    Static table view and choose from gallery option added

---
+++ FoodPin/Controller/NewRestaurantController.swift
@@ -0,0 +1,169 @@
+//
+//  NewRestaurantController.swift
+//  FoodPin
+//
+//  Created by Güray Gül on 5.01.2024.
+//
+
+import UIKit
+
+class NewRestaurantController: UITableViewController {
+
+    @IBOutlet var nameTextField: RoundedTextField! {
+        didSet {
+            nameTextField.tag = 1
+            nameTextField.becomeFirstResponder()
+            nameTextField.delegate = self
+        }
+    }
+    
+    @IBOutlet var typeTextField: RoundedTextField! {
+        didSet {
+            typeTextField.tag = 2
+            typeTextField.delegate = self
+        }
+    }
+    @IBOutlet var addressTextField: RoundedTextField! {
+        didSet {
+            addressTextField.tag = 3
+            addressTextField.delegate = self
+        }
+    }
+    @IBOutlet var phoneTextField: RoundedTextField! {
+        didSet {
+            phoneTextField.tag = 4
+            phoneTextField.delegate = self
+        }
+    }
+    @IBOutlet var descriptionTextView: UITextView! {
+        didSet {
+            descriptionTextView.tag = 5
+            descriptionTextView.layer.cornerRadius = 10.0
+            descriptionTextView.layer.masksToBounds = true
+        }
+    }
+    
+    @IBOutlet var photoImageView: UIImageView! {
+        didSet {
+            photoImageView.layer.cornerRadius = 10.0
+            photoImageView.layer.masksToBounds = true
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Customize the navigation bar appearance
+        if let appearance = navigationController?.navigationBar.standardAppearance {
+            
+            if let customFont = UIFont(name: "Nunito-Bold", size: 40.0) {
+                
+                appearance.titleTextAttributes = [.foregroundColor: UIColor(named: "NavigationBarTitle")!]
+                appearance.largeTitleTextAttributes = [.foregroundColor: UIColor(named: "NavigationBarTitle")!, .font: customFont]
+            }
+            
+            navigationController?.navigationBar.standardAppearance = appearance
+            navigationController?.navigationBar.compactAppearance = appearance
+            navigationController?.navigationBar.scrollEdgeAppearance = appearance
+            
+        }
+        
+        // Get the superview's layout
+        let margins = photoImageView.superview!.layoutMarginsGuide
+        
+        // Disable auto resizing mask to use auto layout programmatically
+        photoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Pin the leading edge of the image view to the margin's leading edge
+        photoImageView.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        
+        // Pin the trailing edge of the image view to the margin's leading edge
+        photoImageView.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        
+        // Pin the top edge of the image view to the margin's top  edge
+        photoImageView.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
+        
+        // Pin the bottom edge of the image view to the margin's bottom  edge
+        photoImageView.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
+        
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+            let photoSourceRequestController = UIAlertController(title: "", message: "Choose your photo source", preferredStyle: .actionSheet)
+            
+            let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { (action) in
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    
+                    let imagePicker = UIImagePickerController()
+                    imagePicker.delegate = self
+                    imagePicker.allowsEditing = false
+                    imagePicker.sourceType = .camera
+                    
+                    self.present(imagePicker, animated: true, completion: nil)
+                }
+            })
+            
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default, handler: { (action) in
+                if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                    
+                    let imagePicker = UIImagePickerController()
+                    imagePicker.delegate = self
+                    imagePicker.allowsEditing = false
+                    imagePicker.sourceType = .photoLibrary
+                    
+                    self.present(imagePicker, animated: true, completion: nil)
+                }
+            })
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            photoSourceRequestController.addAction(cameraAction)
+            photoSourceRequestController.addAction(photoLibraryAction)
+            photoSourceRequestController.addAction(cancelAction)
+            
+            // For Ipad
+            if let popoverController = photoSourceRequestController.popoverPresentationController {
+                if let cell = tableView.cellForRow(at: indexPath) {
+                    popoverController.sourceView = cell
+                    popoverController.sourceRect = cell.bounds
+                }
+            }
+            
+            present(photoSourceRequestController, animated: true, completion: nil)
+            
+        }
+    }
+    
+}
+
+extension NewRestaurantController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let nextTextField = view.viewWithTag(textField.tag + 1) {
+            textField.resignFirstResponder()
+            nextTextField.becomeFirstResponder()
+        }
+        return true
+    }
+}
+
+extension NewRestaurantController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            
+            photoImageView.image = selectedImage
+            photoImageView.contentMode = .scaleAspectFill
+            photoImageView.clipsToBounds = true
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
--- FoodPin/Controller/RestaurantTableViewController.swift
+++ FoodPin/Controller/RestaurantTableViewController.swift
@@ -194,4 +194,9 @@ class RestaurantTableViewController: UITableViewController {
             }
         }
     }
+    
+    @IBAction func unwindToHome(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
