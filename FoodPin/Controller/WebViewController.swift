e189985d17ea6192d5226f6859306e8068aa072b
Güray Gül <guraygul59@gmail.com>
2024-01-07 19:18:14 +0300

    Added WKWebView and SFSafariViewController

---
+++ FoodPin/Controller/WebViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WebViewController.swift
+//  FoodPin
+//
+//  Created by Güray Gül on 7.01.2024.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+
+    @IBOutlet var webView: WKWebView!
+    
+    var targetURL = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let url = URL(string: targetURL) {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+        
+    }
+
+}
